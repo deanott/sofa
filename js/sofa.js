784e82759d7e15e21cfd77857ab05cdc1a6363b2
deanott <dean.ottewell@diamond.ac.uk>
2017-02-26 01:32:53 +0000

    Playin with three

--- js/sofa.js
+++ js/sofa.js
@@ -1,18 +1,47 @@
-/*Logic to fix sofa issue goes here*/
-var sofaDimensions = {
-    paths: 4,
-    width: 50,
-    length: 10
-};
+/*Logic to remove sofa from stairway*/
 
+var sofaDimensins = {
+  width: 300,
+  height: 100,
+  sideRestHeight: 40
+}
 
-function createSofa(width, height) {
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+
+    geometry = new THREE.BoxGeometry(sofaDimensins, 200, 200);
+    material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+        wireframe: true
+    });
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
 
 }
 
+function animate() {
 
-function run() {
+    requestAnimationFrame(animate);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+
+    renderer.render(scene, camera);
+
 }
-
-
-run();
