a1fb77f2238c5f3e1ed352bccfb33d507c23f224
deanott <dean.ottewell@diamond.ac.uk>
2017-02-26 02:32:49 +0000

    well i made a sofa... what am I doing with my life?

--- js/sofa.js
+++ js/sofa.js
@@ -2,11 +2,11 @@
 
 var sofaSize = {
   width: 300,
-  height: 100,
+  height: 75,
   depth: 100,
 
-  armWidth: 40,
-  armHeight: 140,
+  armWidth: 20,
+  armHeight: 123,
 
   backHeight: 150,
   backDepth: 20
@@ -29,17 +29,21 @@ function init() {
 
     coreSofa = new THREE.BoxGeometry(sofaSize.width, sofaSize.height, sofaSize.depth);
     backRestSofa = new THREE.BoxGeometry(sofaSize.width,sofaSize.backHeight,sofaSize.backDepth);
-    armRestSofaLeft = new THREE.BoxGeometry(sofaSize.armWidth,sofaSize.armHeight,sofaSize.depth);
-    armRestSofaRight = new THREE.BoxGeometry(sofaSize.armWidth,sofaSize.armHeight,sofaSize.depth);
+    armRestSofaLeft = new THREE.BoxGeometry(sofaSize.armWidth,sofaSize.armHeight,sofaSize.depth + sofaSize.backDepth);
+    armRestSofaRight = new THREE.BoxGeometry(sofaSize.armWidth,sofaSize.armHeight,sofaSize.depth + sofaSize.backDepth);
 
     //Offset back
     for(var i = 0 ; i < backRestSofa.vertices.length; i++) backRestSofa.vertices[i].z -= sofaSize.depth/2 + sofaSize.backDepth/2;
     for(var i = 0 ; i < backRestSofa.vertices.length; i++) backRestSofa.vertices[i].y -= sofaSize.height/2 - sofaSize.backHeight/2;
 
     //Offset Rests
-    for(var i = 0 ; i < backRestSofa.vertices.length; i++) backRestSofa.vertices[i].y -=  sofaSize.height/2 - sofaSize.armHeight/2;
-    for(var i = 0 ; i < backRestSofa.vertices.length; i++) backRestSofa.vertices[i].x -=  sofaSize.width/2 - sofaSize.armWidth/2;
+    for(var i = 0 ; i < armRestSofaLeft.vertices.length; i++) armRestSofaLeft.vertices[i].y -=  sofaSize.height/2 - sofaSize.armHeight/2;
+    for(var i = 0 ; i < armRestSofaLeft.vertices.length; i++) armRestSofaLeft.vertices[i].x -=  sofaSize.width/2 + sofaSize.armWidth/2;
+    for(var i = 0 ; i < armRestSofaLeft.vertices.length; i++) armRestSofaLeft.vertices[i].z -=  sofaSize.backDepth/2;
 
+    for(var i = 0 ; i < armRestSofaRight.vertices.length; i++) armRestSofaRight.vertices[i].y -=  sofaSize.height/2 - sofaSize.armHeight/2;
+    for(var i = 0 ; i < armRestSofaRight.vertices.length; i++) armRestSofaRight.vertices[i].x +=  sofaSize.width/2 + sofaSize.armWidth/2;
+    for(var i = 0 ; i < armRestSofaRight.vertices.length; i++) armRestSofaRight.vertices[i].z -=  sofaSize.backDepth/2;
 
     material = new THREE.MeshBasicMaterial({
         color: 0xff0000,
@@ -48,14 +52,13 @@ function init() {
 
     meshCore = new THREE.Mesh(coreSofa, material);
     meshBack = new THREE.Mesh(backRestSofa, material);
-
-
-    meshRestLeft = new THREE.Mesh(armRestSofa, material);
-
+    meshRestLeft = new THREE.Mesh(armRestSofaLeft, material);
+    meshRestRight = new THREE.Mesh(armRestSofaRight, material);
 
     sofa.merge(meshCore.geometry, meshCore.matrix);
-
     sofa.merge(meshBack.geometry, meshBack.matrix);
+    sofa.merge(meshRestLeft.geometry, meshRestLeft.matrix);
+    sofa.merge(meshRestRight.geometry, armRestSofaRight.matrix);
 
     mesh = new THREE.Mesh(sofa, material);
     scene.add(mesh);
