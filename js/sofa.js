72332ad69c7e2577ec63ef036009bacc53b9f72f
deanott <dean.ottewell@diamond.ac.uk>
2017-02-26 01:00:27 +0000

    intital setup

---
+++ js/sofa.js
@@ -0,0 +1,18 @@
+/*Logic to fix sofa issue goes here*/
+var sofaDimensions = {
+    paths: 4,
+    width: 50,
+    length: 10
+};
+
+
+function createSofa(width, height) {
+
+}
+
+
+function run() {
+}
+
+
+run();
